e99de7cb668463202cbdebd7aa8c459b96184ce4
maxxir <mr.maxim.ibragimov@gmail.com>
2019-01-18 22:37:28 +0400

    Add double-IP config for [05_m1284p_WIZNET_DNS_client]

--- 05_m1284p_WIZNET_DNS_client/main.c
+++ 05_m1284p_WIZNET_DNS_client/main.c
@@ -220,13 +220,25 @@ uint16_t adc_read(uint8_t channel)
 unsigned char ethBuf0[ETH_MAX_BUF_SIZE];
 unsigned char ethBuf1[ETH_MAX_BUF_SIZE];
 
+//#define IP_WORK
+
+#ifdef IP_WORK
+//NIC metrics for WORK PC
 wiz_NetInfo netInfo = { .mac  = {0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef}, // Mac address
 		.ip   = {192, 168, 0, 199},         // IP address
 		.sn   = {255, 255, 255, 0},         // Subnet mask
 		.dns =  {8,8,8,8},			  // DNS address (google dns)
 		.gw   = {192, 168, 0, 1}, // Gateway address
-		.dhcp = NETINFO_STATIC};    //Dynamic IP configruation from a DHCP sever
-
+		.dhcp = NETINFO_STATIC};    //Dynamic IP configuration from a DHCP sever
+#else
+//NIC metrics for another PC (second IP configuration)
+wiz_NetInfo netInfo = { .mac  = {0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef}, // Mac address
+		.ip   = {192, 168, 1, 199},         // IP address
+		.sn   = {255, 255, 255, 0},         // Subnet mask
+		.dns =  {8,8,8,8},			  // DNS address (google dns)
+		.gw   = {192, 168, 1, 1}, // Gateway address
+		.dhcp = NETINFO_STATIC};    //Dynamic IP configuration from a DHCP sever
+#endif
 void cs_sel() {
 	SPI_WIZNET_ENABLE();
 }
