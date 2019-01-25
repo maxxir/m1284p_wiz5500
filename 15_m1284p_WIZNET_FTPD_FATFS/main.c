ed8e1fce06d509ab759235fb1908f7c02ecef4dd
maxxir <mr.maxim.ibragimov@gmail.com>
2019-01-25 23:32:09 +0400

    Add double-IP config for [15_m1284p_WIZNET_FTPD_FATFS]

--- 15_m1284p_WIZNET_FTPD_FATFS/main.c
+++ 15_m1284p_WIZNET_FTPD_FATFS/main.c
@@ -27,13 +27,23 @@
 #define CHK_RAM_LEAKAGE
 #define CHK_UPTIME
 
-//NIC metrics
+#ifdef IP_WORK
+//NIC metrics for WORK PC
 wiz_NetInfo netInfo = { .mac  = {0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef}, // Mac address
 		.ip   = {192, 168, 0, 199},         // IP address
 		.sn   = {255, 255, 255, 0},         // Subnet mask
 		.dns =  {8,8,8,8},			  // DNS address (google dns)
 		.gw   = {192, 168, 0, 1}, // Gateway address
 		.dhcp = NETINFO_STATIC};    //Static IP configuration
+#else
+//NIC metrics for another PC (second IP configuration)
+wiz_NetInfo netInfo = { .mac  = {0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef}, // Mac address
+		.ip   = {192, 168, 1, 199},         // IP address
+		.sn   = {255, 255, 255, 0},         // Subnet mask
+		.dns =  {8,8,8,8},			  // DNS address (google dns)
+		.gw   = {192, 168, 1, 1}, // Gateway address
+		.dhcp = NETINFO_STATIC};    //Static IP configuration
+#endif
 
 //uint8_t gFTPBUF[_MAX_SS_FTPD + 988]; //1500 bytes - Don't need since v1.3
 uint8_t gFTPBUF[_MAX_SS_FTPD]; //512 bytes
