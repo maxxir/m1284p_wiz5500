a1fb5014cd19a1facd46a5ed18954e3694d85052
maxxir_w <maxx_ir@rambler.ru>
2019-03-25 09:17:13 +0400

    Updated SPI code driver

--- 19_m1284p_WIZNET_blynk/globals.h
+++ 19_m1284p_WIZNET_blynk/globals.h
@@ -41,6 +41,10 @@ static FATFS Fatfs;		//File system object for each logical drive. >= 2
 
 #define IP_WORK
 
+//SPI CLOCK 4 or 8Mhz
+#define SPI_4_MHZ
+//#define SPI_8_MHZ
+
 extern unsigned long millis(void);
 extern int freeRam (void);
 
